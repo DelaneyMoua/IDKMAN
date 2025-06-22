73920c7dc6a447345748d4b58e81eb610f3cdb7f
DelaneyMoua <delaneymoua@gmail.com>
2025-06-22 22:55:04 +0000

    idkman

--- script.js
+++ script.js
@@ -6,6 +6,12 @@ background(110);
 
 function draw() //this runs a loop to update display
 {
-
+point(150,250);
+line(0,400,400,0); /*x1 and y1 is the first point of the line,
+and then x2 and y2 is the ending point of the line */
+rect(0,0,100,100);
+rect(100,100,100,100);
+rect(200,200,100,100);
+rect(300,300,100,100);
 }
 
